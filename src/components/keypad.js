6f324b4756987d15a18c1c44ee0be5c74a8a40e0
PlayDMaster <scottlee0483@gmail.com>
2019-05-14 21:49:25 -0600

    added button functionality

--- src/components/keypad.js
+++ src/components/keypad.js
@@ -25,6 +25,11 @@ export class Keypad extends Component {
         this.setState({ input: '' });
     };
 
+    addNum = ev => {
+        const elem = document.getElementById('kpInput');
+        elem.value = ev.target.value;
+        console.log(elem);
+    }
     render() {
         return (
             <div className='key-pad-box'>
@@ -32,70 +37,70 @@ export class Keypad extends Component {
                     <div className='row'>
                         <div className='col-sm-1'></div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds0'>{this.state.keypadDefinedScores[0]}</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds0' onClick={this.addNum} value={this.state.keypadDefinedScores[0]}>{this.state.keypadDefinedScores[0]}</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kp7' value={7}>7</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kp7' value={7} onClick={this.addNum}>7</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kp8' value={8}>8</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kp8' value={8} onClick={this.addNum}>8</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kp9' value={9}>9</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kp9' value={9} onClick={this.addNum}>9</button>
                         </div>
 
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds4'>{this.state.keypadDefinedScores[4]}</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds4' onClick={this.addNum} value={this.state.keypadDefinedScores[4]}>{this.state.keypadDefinedScores[4]}</button>
                         </div>
                         <div className='col-sm-1'></div>
                     </div>
                     <div className='row'>
                         <div className='col-sm-1'></div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds1'>{this.state.keypadDefinedScores[1]}</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds1' onClick={this.addNum} value={this.state.keypadDefinedScores[1]}>{this.state.keypadDefinedScores[1]}</button>
                         </div>
 
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kp7' value={4}>4</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kp7' value={4} onClick={this.addNum}>4</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kp8' value={5}>5</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kp8' value={5} onClick={this.addNum}>5</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kp9' value={6}>6</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kp9' value={6} onClick={this.addNum}>6</button>
                         </div>
 
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds5'>{this.state.keypadDefinedScores[5]}</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds5' onClick={this.addNum} value={this.state.keypadDefinedScores[5]}>{this.state.keypadDefinedScores[5]}</button>
                         </div>
                         <div className='col-sm-1'></div>
                     </div>
                     <div className='row'>
                         <div className='col-sm-1'></div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds2'>{this.state.keypadDefinedScores[2]}</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds2' onClick={this.addNum} value={this.state.keypadDefinedScores[2]}>{this.state.keypadDefinedScores[2]}</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kp7' value={1}>1</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kp7' value={1} onClick={this.addNum}>1</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kp8' value={2}>2</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kp8' value={2} onClick={this.addNum}>2</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kp9' value={3}>3</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kp9' value={3} onClick={this.addNum}>3</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds6'>{this.state.keypadDefinedScores[6]}</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds6' onClick={this.addNum} value={this.state.keypadDefinedScores[6]}>{this.state.keypadDefinedScores[6]}</button>
                         </div>
                         <div className='col-sm-1'></div>
                     </div>
                     <div className='row'>
                         <div className='col-sm-1'></div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds3'>{this.state.keypadDefinedScores[3]}</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds3' onClick={this.addNum} value={this.state.keypadDefinedScores[3]}>{this.state.keypadDefinedScores[3]}</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kp0' value={1}>0</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kp0' value={1} onClick={this.addNum}>0</button>
                         </div>
                         <div className='col-sm-2 button-div'>
                             <input type='number' min='0' max='180' id='kpInput' onChange={ev => this.updateInput(ev.target.value)} value={this.state.input} />
@@ -104,7 +109,7 @@ export class Keypad extends Component {
                             <button type="button" className="btn btn-outline-light btn-lg" id='kpEnter' onClick={this.handleClick}>Enter</button>
                         </div>
                         <div className='col-sm-2 button-div'>
-                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds7'>{this.state.keypadDefinedScores[7]}</button>
+                            <button type="button" className="btn btn-outline-light btn-lg" id='kpds7' onClick={this.addNum} value={this.state.keypadDefinedScores[7]}>{this.state.keypadDefinedScores[7]}</button>
                         </div>
                         <div className='col-sm-1'></div>
                     </div>
